72cbb4c290c48d3ae5524fec7ab2f9ad9c10298d
Sotiris Evangelou <swtevag@gmail.com>
2019-05-28 16:56:27 +0300

    Update incorrect_constructor.sol

--- contracts_and_results/incorrect_constructor.sol
+++ contracts_and_results/incorrect_constructor.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.15;
 
 contract Missing{
     address private owner;
-
+    uint public balance;
+    
     modifier onlyowner {
         require(msg.sender==owner);
         _;
@@ -10,6 +11,11 @@ contract Missing{
 
     // The name of the constructor should be Missing
     // Anyone can call the IamMissing once the contract is deployed
+    
+    function addToBalance() payable {
+        balance += msg.value;    
+    }
+    
     function IamMissing()
         public 
     {
@@ -20,6 +26,6 @@ contract Missing{
         public 
         onlyowner
     {
-       owner.transfer(this.balance);
+       owner.transfer(balance);
     }
 }
